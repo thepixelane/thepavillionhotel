1c3a775cb18a88467323e940addfbad322590564
sarangk02 <sarang.kulkarni@somaiya.edu>
2026-08-19 17:04:54 +0530

    feat: streamline environment variable retrieval in sanity configuration files

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,7 +4,9 @@
 **/
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_PROJECT_ID || 'cnyln2dk'
+const dataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_DATASET || 'production'
 
 export default defineCliConfig({ api: { projectId, dataset } })
--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,20 +1,11 @@
+const getSanityValue = (publicKey: string, legacyKey: string) =>
+  process.env[publicKey] || process.env[legacyKey] || undefined
+
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-07-24'
+  getSanityValue('NEXT_PUBLIC_SANITY_API_VERSION', 'SANITY_API_VERSION') || '2024-01-01'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset =
+  getSanityValue('NEXT_PUBLIC_SANITY_DATASET', 'SANITY_DATASET') || 'production'
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
-
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage)
-  }
-
-  return v
-}
+export const projectId =
+  getSanityValue('NEXT_PUBLIC_SANITY_PROJECT_ID', 'SANITY_PROJECT_ID') || 'cnyln2dk'
